fdcb40e479cdefdbd65b64d4a0c3f0ebd0867467
danihek <danihek07@gmail.com>
2024-10-19 01:26:05 +0200

    yt-less

---
+++ default.nix
@@ -0,0 +1,63 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix/";
+    ref = "refs/tags/3.0.2";
+  }) {};
+
+  repo = pkgs.fetchgit {
+    url = "https://git.sr.ht/~heckyel/yt-local";
+    rev = "refs/heads/master";
+    outputHash = "sha256-0finwKrnnVY+rGxBjvcG3vKtul5YqH1oIrXze/4GMNQ=";
+  };
+
+  #requirements = builtins.readFile "${repo}/requirements.txt";
+
+  pythonEnv = mach-nix.mkPython {
+    requirements = ''
+      blinker
+      Brotli
+      cachetools
+      click
+      defusedxml
+      Flask
+      gevent
+      greenlet
+      itsdangerous
+      Jinja2
+      MarkupSafe
+      PySocks
+      stem
+      urllib3
+      Werkzeug
+      zope.event
+      zope.interface
+      '';
+  };
+  
+in
+  pkgs.stdenv.mkDerivation rec {
+    pname = "yt-local";
+    version = "0.0.1";
+
+    src = repo;
+
+    buildInputs = [
+      pkgs.git
+      pythonEnv
+      pkgs.python312
+      pkgs.python312Packages.pip
+    ];
+  
+    installPhase = ''
+      cp -r ${repo} $out
+    '';
+  
+    meta = with pkgs.lib; {
+      description = "yt-local for nix";
+      license = licenses.mit;
+      maintainers = [ maintainers.danihek ];
+      platforms = platforms.all;
+    };
+  }
